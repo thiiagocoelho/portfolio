59dfe7e26e00d4442376edd950356c0369c18211
thiiagocoelho <thiagoukx2@gmail.com>
2026-01-06 11:26:31 -0300

    Add: new projet

--- scripts/data.js
+++ scripts/data.js
@@ -1,19 +1,20 @@
 export const meusProjetos = [
     {
         id: 1,
-        titulo: "Green House",
-        descricao: "Landing page desenvolvida para um projeto fictício de uma empresa de jardinagem chamada Green House. O objetivo é apresentar os serviços da empresa de forma clara e atraente, utilizando um design responsivo e acessível.",
+        titulo: "Produtos Paraíso",
+        descricao: "Landing page desenvolvida para treino em HTML, CSS e Figma, com o objetivo de praticar habilidades de design responsivo e criação de layouts atraentes para promover produtos da marca 'Produtos Paraíso'.",
         tecnologias: [
             { nome: 'HTML5', icone: 'fab fa-html5' },
             { nome: 'CSS3', icone: 'fab fa-css3-alt' },
             { nome: 'JavaScript', icone: 'fab fa-js-square' },
             { nome: 'Figma', imagemIcone: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/256px-Figma-logo.svg.png' }
         ],
-        linkOnline: "https://green-house-thk.netlify.app/",
+        linkOnline: "https://paraiso-lake.vercel.app/",
         imagensGaleria: [
-            "../paginaProjetos/imgProjetos/Captura de tela 2025-07-08 115308.png",
-            "../paginaProjetos/imgProjetos/Captura de tela 2025-07-09 095146.png",
-            "../paginaProjetos/imgProjetos/Captura de tela 2025-07-09 095211.png"
+            "../paginaProjetos/imgProjetos/Paraiso-home.png",
+            "../paginaProjetos/imgProjetos/Paraiso-products.png",
+            "../paginaProjetos/imgProjetos/Paraiso-mission.png",
+            "../paginaProjetos/imgProjetos/Paraiso-contact.png"
         ]
     },
     {
